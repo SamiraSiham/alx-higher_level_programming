316db998b5df1945e7203e7a917b86dcab1c270e
SamiraSiham <samirasiham.school2021@gmail.com>
2024-03-18 17:39:20 -0700

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max = 0
-    for i in my_list:
-        if i > my_list:
-            max = i
-    return max
+    my_list.sort()
+    return my_list[-1]
