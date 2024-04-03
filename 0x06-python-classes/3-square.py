840598f09e3bc8da422d55982333473400694a43
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-02 18:50:55 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square Class Documentation"""
+
+
+class Square:
+    """Define class attributes"""
+
+    def __init__(self, size=0):
+        """Constructor:
+
+        Args:
+            size: length of square side
+
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size is a negative number
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    
+    def area(self):
+        """Area of square
+
+        Returns:
+            size squared
+        """
+        return self.__size ** 2
