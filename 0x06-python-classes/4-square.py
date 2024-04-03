e80773bebbbde399e631345e75e142642f69611c
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-02 18:58:24 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -42,7 +42,7 @@ class Square:
         Returns:
             none
         """
-        if value is not int:
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
