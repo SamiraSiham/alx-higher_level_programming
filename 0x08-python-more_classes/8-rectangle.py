0445364a19b6ad14aff8a978270a67cfdf7d76c1
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-08 19:44:09 -0700

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -102,6 +102,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         '''Returns the bigger of two rectangles
         Args:
