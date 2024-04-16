d5dc19a87981300d0d36d7b70f6139428eb9c1fa
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-16 08:51:03 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-	'''read filename with utf-8'''
-	with open(filename, encoding='utf-8') as f:
-		print(f.read, end="")
+    '''read filename with utf-8'''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read, end="")
