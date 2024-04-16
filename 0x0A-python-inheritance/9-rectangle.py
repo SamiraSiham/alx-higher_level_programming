b180c827c94951e24c07dd2842a4233f84b11e47
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-16 05:24:43 -0700

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module for rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''A subclass that represents a rectangle'''
+    def __init__(self, width, height):
+        '''constructor'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        return self.__width * self.__height
+    def __str__(self):
+        return f'[Rectangle] {self.__width}/{self.__height}'
