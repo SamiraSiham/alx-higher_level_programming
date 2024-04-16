ae1731a0c604e34fe254259b3cfa5a2823ccd36b
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-15 17:18:18 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+
+def inherits_from(obj, a_class):
+    '''Determines if an object is a tru subclass of a class.'''
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
