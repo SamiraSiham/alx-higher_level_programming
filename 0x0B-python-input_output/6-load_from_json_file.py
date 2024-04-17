e66db1fa6fe15ac798d48967dbdca42bc27820c1
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-16 18:22:27 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''create object from json file'''
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
