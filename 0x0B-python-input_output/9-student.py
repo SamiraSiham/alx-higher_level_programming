55b22fc3bcc4048df4e7cb7797ae48c8b92c7fda
SamiraSiham <samirasiham.school2021@gmail.com>
2024-04-16 18:45:45 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     '''representing a student'''
-    def __init__(sef, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
