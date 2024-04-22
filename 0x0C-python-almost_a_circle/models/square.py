c4353987618eaa8d3871d1f6074c41c8b4d95e1e
Samira Siham <samirasiham.school2021@gmail.com>
2024-04-22 15:01:46 -0700

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
         '''Constructor.'''
         super().__init__(size, size, x, y, id)
 
-        def __str__(self):
-            '''Returns string info about this square.'''
+    def __str__(self):
+        '''Returns string info about this square.'''
             return '[{}] ({}) {}/{} - {}'.\
                 format(type(self).__name__, self.id, self.x, self.y,
                        self.width)
