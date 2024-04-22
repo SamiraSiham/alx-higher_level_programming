31166197882a616bcbf85cbfa566c8a8d602f4cc
Samira Siham <samirasiham.school2021@gmail.com>
2024-04-22 15:10:35 -0700

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,35 +16,35 @@ class Square(Rectangle):
             format(type(self).__name__, self.id, self.x, self.y,
                        self.width)
 
-        @property
-        def size(self):
-            '''Size of this square.'''
-            return self.width
+    @property
+    def size(self):
+        '''Size of this square.'''
+        return self.width
 
-        @size.setter
-        def size(self, value):
-            self.width = value
-            self.height = value
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
-        def __update(self, id=None, size=None, x=None, y=None):
-            '''Internal method that updates attributes via */**args.'''
-            if id is not None:
-                self.id = id
-            if size is not None:
-                self.size = size
-            if x is not None:
-                self.x = x
-            if y is not None:
-                self.y = y
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method that updates attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
-        def update(self, *args, **kwargs):
-            '''Updates instance attributes via no-keyword & keyword args.'''
-            if args:
-                self.__update(*args)
-            elif kwargs:
-                self.__update(**kwargs)
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
-        def to_dictionary(self):
-            '''Returns dictionary representation of this class.'''
-            return {"id": self.id, "size": self.width,
-                    "x": self.x, "y": self.y}
+    def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
